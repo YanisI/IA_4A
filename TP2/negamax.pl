71e968ed4f8ea07078c7140caf24ebbfd8c506be
jandrieu11 <jonathan.andrieu11@gmail.com>
2020-03-27 10:40:47 +0100

    negamax ok

--- TP2/negamax.pl
+++ TP2/negamax.pl
@@ -1,96 +1,89 @@
-	/*
+/*
 	Ce programme met en oeuvre l'algorithme Minmax (avec convention
 	negamax) et l'illustre sur le jeu du TicTacToe (morpion 3x3)
 	*/
-	
+
 :- [tictactoe].
 
 
 	/****************************************************
-  	ALGORITHME MINMAX avec convention NEGAMAX : negamax/5
-  	*****************************************************/
+	ALGORITHME MINMAX avec convention NEGAMAX : negamax/5
+	*****************************************************/
 
 	/*
 	negamax(+J, +Etat, +P, +Pmax, [?Coup, ?Val])
-
 	SPECIFICATIONS :
-
 	retourne pour un joueur J donne, devant jouer dans
 	une situation donnee Etat, de profondeur donnee P,
 	le meilleur couple [Coup, Valeur] apres une analyse
 	pouvant aller jusqu'a la profondeur Pmax.
-
 	Il y a 3 cas a decrire (donc 3 clauses pour negamax/5)
-	
+
 	1/ la profondeur maximale est atteinte : on ne peut pas
-	developper cet Etat ; 
+	developper cet Etat ;
 	il n'y a donc pas de coup possible a jouer (Coup = rien)
 	et l'evaluation de Etat est faite par l'heuristique.
-
 	2/ la profondeur maximale n'est pas  atteinte mais J ne
 	peut pas jouer ; au TicTacToe un joueur ne peut pas jouer
 	quand le tableau est complet (totalement instancie) ;
 	il n'y a pas de coup a jouer (Coup = rien)
 	et l'evaluation de Etat est faite par l'heuristique.
-
 	3/ la profondeur maxi n'est pas atteinte et J peut encore
-	jouer. Il faut evaluer le sous-arbre complet issu de Etat ; 
-
+	jouer. Il faut evaluer le sous-arbre complet issu de Etat ;
 	- on determine d'abord la liste de tous les couples
 	[Coup_possible, Situation_suivante] via le predicat
 	 successeurs/3 (deja fourni, voir plus bas).
-
 	- cette liste est passee a un predicat intermediaire :
 	loop_negamax/5, charge d'appliquer negamax sur chaque
 	Situation_suivante ; loop_negamax/5 retourne une liste de
 	couples [Coup_possible, Valeur]
-
 	- parmi cette liste, on garde le meilleur couple, c-a-d celui
 	qui a la plus petite valeur (cf. predicat meilleur/2);
 	soit [C1,V1] ce couple optimal. Le predicat meilleur/2
 	effectue cette selection.
-
 	- finalement le couple retourne par negamax est [Coup, V2]
 	avec : V2 is -V1 (cf. convention negamax vue en cours).
-
 A FAIRE : ECRIRE ici les clauses de negamax/5
 .....................................
 	*/
 
-	negamax(_,_,P,P,[Coup,Val]):-
-		coup = rien .
-		
-	negamax(J,Etat,P,Pmax,[rien,Val]):-
-		ground(Etat) .
+negamax(J,Etat,P,P,[Coup,Val]):-
+	Coup = rien,
+	heuristique(J,Etat,Val).
 
-	negamax(J,Etat,P,Pmax,[Coup,Val]):-
-		successeur(J,Etat,Suc),
-		loop_negamax(J,P,Pmax,Suc,[[Coup,Vsuiv]|Reste_Couples]),
-		meilleur([[Coup,Vsuiv]|Reste_Couples],Meilleur_couple),
-		Meilleur_couple = [Coup , -Val].
+negamax(J,Etat,P,Pmax,[Coup,Val]):-
+	P<Pmax,
+	Coup = rien,
+	ground(Etat),
+	heuristique(J,Etat,Val).
 
-
+negamax(J,Etat,P,Pmax,[Coup,V2]):-
+	P<Pmax,
+	not(situation_terminale(J,Etat)),
+	successeurs(J,Etat,Suc),
+	loop_negamax(J,P,Pmax,Suc,Liste),
+	meilleur(Liste,[Coup,V1]),
+	V2 is -V1.
 
 	/*******************************************
 	 DEVELOPPEMENT D'UNE SITUATION NON TERMINALE
-	 successeurs/3 
+	 successeurs/3
 	 *******************************************/
 
 	 /*
-   	 successeurs(+J,+Etat, ?Succ)
-
-   	 retourne la liste des couples [Coup, Etat_Suivant]
- 	 pour un joueur donne dans une situation donnee 
+	 successeurs(+J,+Etat, ?Succ)
+	 retourne la liste des couples [Coup, Etat_Suivant]
+	 pour un joueur donne dans une situation donnee
 	 */
 
 successeurs(J,Etat,Succ) :-
-	copy_term(Etat, Etat_Suiv),
-	findall([Coup,B,V
-		    succesB,V
-		    Succ).B,V
+		copy_term(Etat, Etat_Suiv),
+		findall([Coup,Etat_Suiv],
+			successeur(J,Etat_Suiv,Coup),
+			Succ).
 
 	/*************************************
-         Boucle permettant d'appliquer negamax 
+         Boucle permettant d'appliquer negamax
          a chaque situation suivante :
 	*************************************/
 
@@ -100,7 +93,7 @@ successeurs(J,Etat,Succ) :-
 	a partir de la liste des couples [Coup, Situation_Suivante]
 	*/
 
-loop_negamax(_,_, _  ,[],                []).
+loop_negamax(_, _, _ , [], []).
 loop_negamax(J,P,Pmax,[[Coup,Suiv]|Succ],[[Coup,Vsuiv]|Reste_Couples]) :-
 	loop_negamax(J,P,Pmax,Succ,Reste_Couples),
 	adversaire(J,A),
@@ -108,7 +101,6 @@ loop_negamax(J,P,Pmax,[[Coup,Suiv]|Succ],[[Coup,Vsuiv]|Reste_Couples]) :-
 	negamax(A,Suiv,Pnew,Pmax, [_,Vsuiv]).
 
 	/*
-
 A FAIRE : commenter chaque litteral de la 2eme clause de loop_negamax/5,
 	en particulier la forme du terme [_,Vsuiv] dans le dernier
 	litteral ?
@@ -116,48 +108,40 @@ A FAIRE : commenter chaque litteral de la 2eme clause de loop_negamax/5,
 
 	/*********************************
 	 Selection du couple qui a la plus
-	 petite valeur V 
+	 petite valeur V
 	 *********************************/
 
 	/*
 	meilleur(+Liste_de_Couples, ?Meilleur_Couple)
-
 	SPECIFICATIONS :
 	On suppose que chaque element de la liste est du type [C,V]
 	- le meilleur dans une liste a un seul element est cet element
 	- le meilleur dans une liste [X|L] avec L \= [], est obtenu en comparant
-	  X et Y,le meilleur couple de L 
+	  X et Y,le meilleur couple de L
 	  Entre X et Y on garde celui qui a la petite valeur de V.
-
 A FAIRE : ECRIRE ici les clauses de meilleur/2
 	*/
-	
-	meilleur([M|[]],M).
 
-	meilleur(Liste_de_Couples, Meilleur_Couple):-
-		append(B,[H|T],Liste_de_Couples),
-		B = [E1,V1],
-		H = [E2,V2],
-		( V1 < V2 
-		-> 	append(B,T,K) 
-		;	append(H,T,K) 
-		),
-		meilleur(K,Meilleur_Couple).
+meilleur([M|[]],M).
 
-
-
-
-
+meilleur(Liste_de_Couples,Meilleur_Couple):-
+	Liste_de_Couples = [H|T],
+	H = [_,V1],
+	meilleur(T,M),
+	M = [_,V2],
+	(   V1 < V2 ->
+	    Meilleur_Couple = H ;
+		Meilleur_Couple = M 
+	).
 
 	/******************
-  	PROGRAMME PRINCIPAL
-  	*******************/
+	PROGRAMME PRINCIPAL
+	*******************/
 
-main(B,V, Pmax) :-
-	negamax(J, Etat, P, 1, [Coup, Val]),
-	negamax(J, Etat, P, 2, [Coup, Val]),
-	negamax(J, Etat, P, 3, [Coup, Val]).        
-
+	main(B,V,Pmax) :-
+		situation_initiale(Ini),
+		joueur_initial(Jini),
+		negamax(Jini,Ini,1,Pmax,[B,V]).
 
 	/*
 A FAIRE :
@@ -165,4 +149,17 @@ A FAIRE :
 	Pmax = 1, 2, 3, 4 ...
 	Commentez les r�sultats obtenus.
 	*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
